c64a154ca815f9ff3622ebe7533da132a613c06a
Faris Walid <thefarixz@gmail.com>
2024-05-11 01:09:17 +0300

    Update global.js

--- pages/global.js
+++ pages/global.js
@@ -1,20 +1,20 @@
 document.getElementById('product-btn').addEventListener('click', function () {
   document.body.classList.add('blur');
   setTimeout(function () {
-    window.location.href = '../product.html';
+    window.location.href = '../Web-Project/product.html';
   }, 100);
 });
 
 document.getElementById('home-btn').addEventListener('click', function () {
   document.body.classList.add('blur');
   setTimeout(function () {
-    window.location.href = '../main.html';
+    window.location.href = '../Web-Project/main.html';
   }, 100);
 });
 
 document.getElementById('about-btn').addEventListener('click', function () {
   document.body.classList.add('blur');
   setTimeout(function () {
-    window.location.href = '../about.html';
+    window.location.href = '../Web-Project/about.html';
   }, 100);
 });
