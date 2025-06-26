367c16903e1f28431f77124776de10728116c5dc
Faris Walid <108816669+FARIXZ@users.noreply.github.com>
2025-06-26 20:55:58 +0300

    Update global.js

--- pages/global.js
+++ pages/global.js
@@ -1,20 +1,23 @@
+const pathParts = window.location.pathname.split('/');
+const base = '/' + pathParts[1] + '/'; // gets 'Web-Project' in '/Web-Project/page.html'
+
 document.getElementById('product-btn').addEventListener('click', function () {
   document.body.classList.add('blur');
   setTimeout(function () {
-    window.location.href = 'product.html';
+    window.location.href = base + 'product.html';
   }, 100);
 });
 
 document.getElementById('home-btn').addEventListener('click', function () {
   document.body.classList.add('blur');
   setTimeout(function () {
-    window.location.href = 'main.html';
+    window.location.href =  base + 'main.html';
   }, 100);
 });
 
 document.getElementById('about-btn').addEventListener('click', function () {
   document.body.classList.add('blur');
   setTimeout(function () {
-    window.location.href = 'about.html';
+    window.location.href =  base + 'about.html';
   }, 100);
 });
